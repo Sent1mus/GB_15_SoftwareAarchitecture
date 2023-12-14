392da0841682e6af464ac7131263557106ea4eba
Elim Yunusov <elim.yunusov1@gmail.com>
2023-12-15 00:52:12 +0400

    seminar_06

---
+++ seminar_06/06.sql
@@ -0,0 +1,24 @@
+ALTER TABLE `Pacient` DROP FOREIGN KEY `Pacient_fk0`;
+
+ALTER TABLE `Doctor` DROP FOREIGN KEY `Doctor_fk0`;
+
+ALTER TABLE `Doctor` DROP FOREIGN KEY `Doctor_fk1`;
+
+ALTER TABLE `Diagnose` DROP FOREIGN KEY `Diagnose_fk0`;
+
+ALTER TABLE `Diagnose` DROP FOREIGN KEY `Diagnose_fk1`;
+
+ALTER TABLE `Hystory` DROP FOREIGN KEY `Hystory_fk0`;
+
+ALTER TABLE `Hystory` DROP FOREIGN KEY `Hystory_fk1`;
+
+ALTER TABLE `Hystory` DROP FOREIGN KEY `Hystory_fk2`;
+
+ALTER TABLE `Hystory` DROP FOREIGN KEY `Hystory_fk3`;
+
+ALTER TABLE `Hystory` DROP FOREIGN KEY `Hystory_fk4`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk1`;
+

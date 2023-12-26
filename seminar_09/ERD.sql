7f476de469f042048b44de7a2eb65d8ee3256acb
Elim Yunusov <elim.yunusov1@gmail.com>
2023-12-26 16:55:27 +0400

    semianar_09

---
+++ seminar_09/ERD.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk0`;
+
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk1`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk0`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk1`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk2`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk3`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk4`;
+
+ALTER TABLE `Resource` DROP FOREIGN KEY `Resource_fk5`;
+

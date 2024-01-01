05a860bb4752884dfe302742db1ee9d87a3154ae
Elim Yunusov <elim.yunusov1@gmail.com>
2024-01-01 21:35:44 +0400

    seminar_11

---
+++ seminar_11/ERD.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `Robot` DROP FOREIGN KEY `Robot_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk1`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk2`;
+
+ALTER TABLE `History` DROP FOREIGN KEY `History_fk0`;
+
